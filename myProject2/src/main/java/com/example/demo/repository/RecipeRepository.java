eab94a3b3e06d2fae230782505df14dc58e497c6
xabinM <binine2@gmail.com>
2025-05-26 17:04:07 +0900

    refactor : 안쓰는 recipe 관련 클래스 삭제

--- myProject2/src/main/java/com/example/demo/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.recipe.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
